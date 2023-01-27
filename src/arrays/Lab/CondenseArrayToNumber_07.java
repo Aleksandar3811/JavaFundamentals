613532dd6109a8c608471bf32aea47410c3cf891
Aleksandar3811 <alek.kondrov@gmail.com>
2023-01-27 20:58:23 +0200

    This is my exercises for arrays

--- src/arrays/Lab/CondenseArrayToNumber_07.java
+++ src/arrays/Lab/CondenseArrayToNumber_07.java
@@ -4,7 +4,30 @@ import java.util.Scanner;
 
 public class CondenseArrayToNumber_07 {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] inputArr = input.split(" ");
+        int[] nums = new int[inputArr.length];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.parseInt(inputArr[i]);
 
+        }
+        int[] condensedArr = new int[nums.length - 1];
+        for (int j = 0; j< nums.length; j++) {
+            if (nums.length == 1) {
+                break;
+            }
+            if (j== nums.length-1){
+                int[]condensedNew=new int[condensedArr.length-1];
+                j=-1;
+                nums =condensedArr;
+                condensedArr=condensedNew;
+            }
+            else{
+                condensedArr[j]= nums[j]+ nums[j+1];
+
+            }
+        }
+        System.out.println(nums[0]);
     }
 }
